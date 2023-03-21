d267f097eab4363aa4a6ae578af463938ff84e77
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-21 20:28:12 +0300

    21.03.2023

--- src/day15_StringManMethodCreation/C04_MethodCreation.java
+++ src/day15_StringManMethodCreation/C04_MethodCreation.java
@@ -11,7 +11,21 @@ public class C04_MethodCreation {
         int input2=20;
 
         //method 4 adımda oluşturulur
-        //1
+        //1.method call
+        //2.argument eklenmesi gerekiyorsa ekleyelim
+        //  eğer method un return type void den farklı olacaksa
+        //  bir variable olusturup,method call u assign edelim
+
+        topla(input1,input2);
+
+    }
+
+    public static void topla(int input1, int input2) {
+
+       //3.adım:method deklarasyounda değiştirilmesi gereken bolumleri değiştir.(access modifier,return Type vb..)
+       //4. eğer return type void  dışında bir şeyse return keyword u ve donecek değeri hesaplamalıyız
+
+        System.out.println("girilen iki sayının toplamı : " +(input1+input2));
 
     }
 }
