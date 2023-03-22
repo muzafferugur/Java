e8c20d6fb53f7e4ddf446d07bed1c880c298ef22
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-22 16:39:22 +0300

    22.03.2023

---
+++ src/day16_methodCreation/C06_MethodCreation.java
@@ -0,0 +1,8 @@
+package day16_methodCreation;
+
+public class C06_MethodCreation {
+    public static void main(String[] args) {
+
+        System.out.println(C05_MethodCreationReturn.sehirAl());
+    }
+}
