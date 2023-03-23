f458ef4ad758ca2fb7bda5c8fd071e583fb0be26
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-23 18:04:35 +0300

    23.03.2023

--- src/day18_ForLoop/C06_NestedForLoop.java
+++ src/day18_ForLoop/C06_NestedForLoop.java
@@ -11,6 +11,26 @@ public class C06_NestedForLoop {
         * * * * *
          */
         // yan yana 3 tane * yaz
+        for (int i = 1; i <=3 ; i++) {
+            System.out.print("* ");
+
+        }
+
+        for (int i = 1; i <=4 ; i++) {
+            System.out.print("* ");
+
+        }
+        System.out.println("");
+
+        //bu tur durumlarda iç içe(nested) loop kullanmak gerekir
+
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.println("* ");
+
+            }
+
+        }
 
     }
 }
