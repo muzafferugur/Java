bf14270e9330323f20a9048a0cc6f6d3c178eb75
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-23 17:44:21 +0300

    23.03.2023

---
+++ src/day18_ForLoop/C05_ForLoop.java
@@ -0,0 +1,13 @@
+package day18_ForLoop;
+
+public class C05_ForLoop {
+    public static void main(String[] args) {
+
+        /*
+        Soru 10 ) Kullanicidan iki sayi isteyin.
+        Girilen sayilar ve aralarindaki tum tamsayilari toplayip,
+         sonucu yazdiran bir program yaziniz
+         */
+
+    }
+}
