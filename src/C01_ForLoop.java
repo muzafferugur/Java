eaed2d55a225d9beb67ba1bd7938eb50f338316e
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-23 23:52:37 +0300

    23.03.2023

--- src/C01_ForLoop.java
+++ src/C01_ForLoop.java
@@ -6,5 +6,29 @@ public class C01_ForLoop {
         kelimede kullanılan farklı harf sayisini veren bir method yaziniz
          */
 
+        String input="Java her zaman guzel";
+        tekrarsızYap(input);
     }
+
+    public static void tekrarsızYap(String input) {
+        String benzersizInput="";
+
+        String islenecekKelime=input.replaceAll("\\W", "");
+        System.out.print(islenecekKelime.substring(0,1));
+        benzersizInput+=islenecekKelime.substring(0,1);
+
+        for (int i = 0; i < islenecekKelime.length(); i++) {
+
+            if (!benzersizInput.contains(islenecekKelime.substring(i,i+1)));{
+                System.out.print(", "+islenecekKelime.substring(i,i+1));
+                benzersizInput+=islenecekKelime.substring(i,i+1);
+            }
+
+
+        }
+        System.out.println("");
+        System.out.println("inpt :"+input);
+        System.out.println("benzersiz input :"+benzersizInput);
+    }
+
 }
