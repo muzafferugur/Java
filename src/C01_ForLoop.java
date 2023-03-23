0cdcd97d30ee42e0c149510429c04cb255f8aabd
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-23 22:21:38 +0300

    23.03.2023

---
+++ src/C01_ForLoop.java
@@ -0,0 +1,10 @@
+public class C01_ForLoop {
+    public static void main(String[] args) {
+
+        /*
+        Verilen string'deki kullanılan harfleri tekrarsız olarak yazdırıp
+        kelimede kullanılan farklı harf sayisini veren bir method yaziniz
+         */
+
+    }
+}
