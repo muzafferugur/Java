28aa32fe4bebf43d3cde5a992257f89afd0fe77e
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-24 20:56:48 +0300

    24.03.2023

---
+++ src/day20_while_dowhileLoop/C06_DoWhileLoop.java
@@ -0,0 +1,22 @@
+package day20_while_dowhileLoop;
+
+import java.util.Scanner;
+
+public class C06_DoWhileLoop {
+    public static void main(String[] args) {
+
+        /*
+             Soru 4) Kullanicidan toplamak uzere pozitif sayilar isteyin,
+              islemi bitirmek icin 0’a basmasini soyleyin.
+             Kullanici yanlislikla negative sayi girerse o
+             sayiyi dikkate almayin ve “Negatif sayi giremezsiniz” yazdirip basa donun
+             Kullanici 0’a bastiginda toplam kac pozitif sayi girdigini,
+             yanlislikla kac negative sayi girdigini ve girdigi pozitif sayilarin
+             toplaminin kac oldugunu yazdirin.
+               */
+
+        Scanner scan=new Scanner(System.in);
+        int sayı=0;
+
+    }
+}
