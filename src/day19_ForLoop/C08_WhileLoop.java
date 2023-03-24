64c901ea882b57601eb213e34c8b9b8de387ad27
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-24 04:19:05 +0300

    24.03.2023

--- src/day19_ForLoop/C08_WhileLoop.java
+++ src/day19_ForLoop/C08_WhileLoop.java
@@ -1,7 +1,29 @@
 package day19_ForLoop;
 
+import java.util.Scanner;
+
 public class C08_WhileLoop {
     public static void main(String[] args) {
 
+        /*
+        Kullanicidan toplanmak uzere sayilar alın
+        sayılarin toplami 500'ü gecince (500 dahil)
+        sayilarin toplamını ve kaç sayı toplandığını
+        şu şekilde yazdirin
+        13 sayi girdiniz ve toplamlari 567
+         */
+
+        int toplam=0;
+        int sayı=0;
+        int sayac=0;
+        Scanner scan=new Scanner(System.in);
+
+        while(toplam<500){
+            System.out.println("lutfen toplamak için sayı girin");
+            sayı= scan.nextInt();
+            toplam+=sayı;
+            sayac++;
+        }
+        System.out.println(sayac+"sayı girdiniz ve toplamları :"+toplam);
     }
 }
---
+++ src/day19_ForLoop/C09_WhileLoop.java
@@ -0,0 +1,18 @@
+package day19_ForLoop;
+
+public class C09_WhileLoop {
+    public static void main(String[] args) {
+
+         /*
+         Soru 6) Kullanicidan bir sifre girmesini isteyin.
+        Asagidaki sartlari sagliyorsa “Sifre basari ile tanimlandi”,
+        sartlari saglamazsa “Islem basarisiz,Lutfen yeni bir sifre girin” yazdirin
+        ve basarili sifre girinceye kadar tekrar sifre girmesini isteyin.
+        - Ilk harf buyuk harf olmali
+        - Son harf kucuk harf olmali
+        - Sifre bosluk icermemeli
+        - Sifre uzunlugu en az 8 karakter olmali
+         */
+
+    }
+}
