c020babfeeea7883f3fff7690237ff72ef214fc2
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-24 03:03:54 +0300

    24.03.2023

---
+++ src/day19_ForLoop/C04_NestedForLoop.java
@@ -0,0 +1,26 @@
+package day19_ForLoop;
+
+public class C04_NestedForLoop {
+    public static void main(String[] args) {
+        /*
+        Verieln input'a gore yıldızlardan olusan bir ucgen olusturun
+        ornek input = 4 ise
+                *
+                * *
+                * * *
+                * * * *
+         */
+
+        int input=10;
+
+        for (int i = 1; i <=input ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+
+
+    }
+}
---
+++ src/day19_ForLoop/C06_NestedForLoop.java
@@ -0,0 +1,25 @@
+package day19_ForLoop;
+
+public class C06_NestedForLoop {
+    public static void main(String[] args) {
+        /*
+        Verilen string'i asagıdaki gibi yazdiran bir program yazdirin.
+        input = Deniz
+        D
+        De
+        Den
+        Deni
+        Deniz
+         */
+
+        String input="Deniz";
+        for (int i = 1; i <=input.length() ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(input.substring(j-1,j));
+
+            }
+            System.out.println("");
+        }
+
+    }
+}
