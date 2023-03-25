578fe539ab01edd4074b1cc06875067653aec9c0
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-25 22:34:26 +0300

    25.03.2023

--- src/day23_Arrays/C09_binarySearch.java
+++ src/day23_Arrays/C09_binarySearch.java
@@ -9,14 +9,21 @@ public class C09_binarySearch {
         /*
         binarySearch methodu sıralanmış array de aradığımız elementin indexini döndürür
 
-        Ya aradığımız element array de yoksa? -1 döndürür
+        Ya aradığımız element array de yoksa?
         (String de indexOf() bize olmayan elentler için -1 döndürüyordu)
+
+        Aradığımız element array de yoksa Java hem olmadığını,
+        hem olsaydı nerede olacağını döndürür
+
+        olmadığını ifade etmek için - kullanır
+        olsaydı nerede olacağını index değil sıralama kullanır
          */
 
         int[] sayılar={3,5,1,4,7,0};
 
         Arrays.sort(sayılar);
         System.out.println(Arrays.toString(sayılar));//[0, 1, 3, 4, 5, 7]
+        //varsa index yoksa -sıra
 
         System.out.println(Arrays.binarySearch(sayılar,1));//1
 
