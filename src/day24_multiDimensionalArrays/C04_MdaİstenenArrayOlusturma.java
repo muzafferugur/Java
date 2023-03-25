05e6cb416fbeddb4bdfd5f924f8076e5bc6a3c0e
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-26 02:52:45 +0300

    26.03.2023

--- src/day24_multiDimensionalArrays/C04_MdaİstenenArrayOlusturma.java
+++ src/day24_multiDimensionalArrays/C04_MdaİstenenArrayOlusturma.java
@@ -1,5 +1,7 @@
 package day24_multiDimensionalArrays;
 
+import java.util.Arrays;
+
 public class C04_MdaİstenenArrayOlusturma {
     public static void main(String[] args) {
 
@@ -8,9 +10,24 @@ public class C04_MdaİstenenArrayOlusturma {
         ic array’lerindeki tum elemanlarin toplamini  birer birer bulan
         ve herbir sonucu yeni bir array’in elemani yapan ve
         yeni array’i  ekrana  yazdiran bir program yaziniz
-        Ornek; { {1,2,3}, {4,5}, {6,7} } ==> 1+2+3=6 4+5=9 6+7=13 ==> output: {6, 9, 13}
+        Ornek; { {1,2,3}, {4,5}, {6,7} } ==> 1+2+3=6 4+5=9 6+7=13
+         ==> output: {6, 9, 13}
          */
+        int[][] input= {{1, 2, 3}, {4, 5}, {6, 7}};
+        int[] yeniArr=new int[input.length];
+        int toplam=0;
 
+        for (int i = 0; i < input.length; i++) {// dıs loop outer array'i kontrol ediyor
+            for (int j = 0; j < input[i].length; j++) {// ic loop inner array'i kontrol ediyor
+
+                toplam+=input[i][j];
+
+            }
+            yeniArr[i]=toplam;
+            toplam=0;
+
+        }
+        System.out.println("istenen yeni array : "+ Arrays.toString(yeniArr));
 
     }
 }
