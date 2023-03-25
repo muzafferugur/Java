2faa3b70fef78f2c77e2e314bfdeb0a97acae5b0
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-26 01:32:53 +0300

    26.03.2023

--- src/day24_multiDimensionalArrays/C02_MdaTumElementleriYazdırma.java
+++ src/day24_multiDimensionalArrays/C02_MdaTumElementleriYazdırma.java
@@ -1,4 +1,28 @@
 package day24_multiDimensionalArrays;
 
 public class C02_MdaTumElementleriYazdırma {
+    public static void main(String[] args) {
+         /*
+        Verilen bir multi-dimensional array'in tum elementlerini yazdiran bir
+        method olusturun.
+         */
+
+        int[][] sayilar = {{1, 5, 6, 9, 4}, {2, 5, 5, 8}, {3, 1, 6}};
+
+        elemetleriYazdır(sayilar);
+
+
+    }
+
+    public static void elemetleriYazdır(int[][] sayilar) {
+
+        for (int i = 0; i < sayilar.length; i++) {//i =0,1,2 değerleri alacak
+            for (int j = 0; j < sayilar[i].length; j++) { //inner arraylerin uzunluğuna bağladık
+                System.out.print(sayilar[i][j]+" ");
+
+            }
+
+        }
+
+    }
 }
---
+++ src/day24_multiDimensionalArrays/C03_MdaIstenenSayilariYazdirma.java
@@ -0,0 +1,12 @@
+package day24_multiDimensionalArrays;
+
+public class C03_MdaIstenenSayilariYazdirma {
+     /*
+        Verilen 2 katli bir multi-dimensional array'de outer index'i ve inner index'i
+        ayni olan sayilarin toplamini bulunuz.
+        int[][] sayilar = {{1, 5, 6, 9, 4}, {2, 5, 5, 8}, {3, 1, 6}};
+        [0][0] + [1][1] + [2][2] .......
+         */
+
+    int[][] sayilar = {{1, 5, 6, 9, 4}, {2, 5, 5, 8}, {3, 1, 6}, {1, 1, 1, 1, 1}};
+}
