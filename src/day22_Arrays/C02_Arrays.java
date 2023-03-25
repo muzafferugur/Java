108f4aa90575e7624368e811c2282a8e41989568
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-25 14:02:11 +0300

    25.03.2023

--- src/day22_Arrays/C02_Arrays.java
+++ src/day22_Arrays/C02_Arrays.java
@@ -1,9 +1,29 @@
 package day22_Arrays;
 
+import java.util.Arrays;
+
 public class C02_Arrays {
     public static void main(String[] args) {
 
         int sayılar []=new int[3];
         System.out.println(sayılar);//[I@19dfb72a
+        //non-primitive data türündeki dataları
+        //her zaman direkt yazdırmayabilir
+        //Array i yazdırmak istersek Arrays classından yardım isteriz
+
+        System.out.println(Arrays.toString(sayılar));//[0, 0, 0]
+
+
+        sayılar[2]=10;
+        sayılar[0]=5;
+        sayılar[1]=3;
+        System.out.println(sayılar);//[I@19dfb72a
+        System.out.println(Arrays.toString(sayılar));
+
+        String sınıfList[]={"Ali","Ayse","Ahmet"};
+        System.out.println(Arrays.toString(sınıfList));//[Ali, Ayse, Ahmet]
+        sınıfList[1]="Hasan";
+        System.out.println(Arrays.toString(sınıfList));//[Ali, Hasan, Ahmet]
+
     }
 }
