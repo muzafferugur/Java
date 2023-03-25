16aa48214a37db0b291f5dbd39bb34efc5935310
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-26 01:13:37 +0300

    26.03.2023

---
+++ src/day24_multiDimensionalArrays/C01_Mda.java
@@ -0,0 +1,20 @@
+package day24_multiDimensionalArrays;
+
+public class C01_Mda {
+    public static void main(String[] args) {
+
+        /*
+        Tek katlı arraylerde array'i direkt yazdıramıyoruz
+        cunku array non-primitive data turudur ve her
+        non-primitive data direkt yazdırılamayabilir
+        
+        ancak array'in içerisindeki elementleri direkt yazdırabiliyorduk
+        cunku genelde primitive data turu veya String elementler kullanılıyordu
+
+        Multi-dimensional arraylerde en dikkat edeceğimiz konu
+        ulaşmak istediğimiz elementin bir array mi yoksa primitive
+        data mı olduğudur
+         */
+
+    }
+}
