120480931cdc073c0d9ead82fd0b4146490207c2
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-26 02:10:21 +0300

    26.03.2023

---
+++ src/day24_multiDimensionalArrays/C04_MdaİstenenArrayOlusturma.java
@@ -0,0 +1,16 @@
+package day24_multiDimensionalArrays;
+
+public class C04_MdaİstenenArrayOlusturma {
+    public static void main(String[] args) {
+
+        /*
+        Soru 4) Asagidaki multi dimensional array’in
+        ic array’lerindeki tum elemanlarin toplamini  birer birer bulan
+        ve herbir sonucu yeni bir array’in elemani yapan ve
+        yeni array’i  ekrana  yazdiran bir program yaziniz
+        Ornek; { {1,2,3}, {4,5}, {6,7} } ==> 1+2+3=6 4+5=9 6+7=13 ==> output: {6, 9, 13}
+         */
+
+
+    }
+}
