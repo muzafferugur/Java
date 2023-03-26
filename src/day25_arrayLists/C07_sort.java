c89b1f6bd340fb8ce08c41a0ad5611e483ae9779
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-27 02:12:58 +0300

    27.03.2023

---
+++ src/day25_arrayLists/C07_sort.java
@@ -0,0 +1,17 @@
+package day25_arrayLists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class C07_sort {
+    public static void main(String[] args) {
+        List<String> urunler = new ArrayList<String>();
+        urunler.add("Nutella");
+        urunler.add("Ikram");
+        urunler.add("Cekirdek");
+        urunler.add("Cay");
+
+        Collections.sort(urunler);
+    }
+}
