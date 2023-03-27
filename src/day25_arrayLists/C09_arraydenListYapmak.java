014ab44cf99672793955ad1216e7174c1628d07d
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-27 21:32:27 +0300

    27.03.2023

--- src/day25_arrayLists/C09_arraydenListYapmak.java
+++ src/day25_arrayLists/C09_arraydenListYapmak.java
@@ -1,5 +1,9 @@
 package day25_arrayLists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class C09_arraydenListYapmak {
     public static void main(String[] args) {
         /*
@@ -11,5 +15,47 @@ public class C09_arraydenListYapmak {
         Bu kadar cok elemanli bir listeyi tek tek eleman olarak girmek yerine
         array olusturup, for loop ile olusturdugumuz list'e tasiyabiliriz.
          */
+
+        List<Integer> sayılar=new ArrayList<Integer>();
+
+        for (int i = 0; i < args.length; i++) {
+            sayılar.add(arr[i]);
+
+        }
+        System.out.println(sayılar);
+        /*
+        bu islemi yapması icin Java'nin olusturdugu bir method da var
+        ancak bu yontemin cok yan etkisi vardir.
+            1- bu sekilde olusturulan listede add, remove gibi size'i degistiren method'lar
+            KULLANILMAZ.
+            2- asList method'u ile olusturulan liste ve kaynak olan array birbiri ile
+            ilisik olarak hayatlarina devam ederler. birinde yaptigimiz deigisiklik
+            otamatik olarak digerine de isler.
+         */
+
+        List <Integer> sayilar2 = Arrays.asList(arr);
+        System.out.println(sayilar2); // [2, 3, 5, 6, 8, 4, 2, 6, 9, 0, 1, 3, 6, 4, 2, 7, 0]
+
+        sayılar.add(5);
+        System.out.println(sayılar); //[2, 3, 5, 6, 8, 4, 2, 6, 9, 0, 1, 3, 6, 4, 2, 7, 0, 5]
+
+        /*
+        sayilar2.add(5);
+        System.out.println(sayilar2); //UnsupportedOperationException
+         */
+
+
+        arr[0] = 20;
+        System.out.println(Arrays.toString(arr));
+        // [20, 3, 5, 6, 8, 4, 2, 6, 9, 0, 1, 3, 6, 4, 2, 7, 0]
+
+        System.out.println("Array'de degisiklik yapinca sayilar2 : "+ sayilar2);
+        //Array'de degisiklik yapinca sayilar2 : [20, 3, 5, 6, 8, 4, 2, 6, 9, 0, 1, 3, 6, 4, 2, 7, 0]
+
+
+
+
+
+
     }
 }
