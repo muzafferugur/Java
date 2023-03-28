96a29d73f07bfc9ef4075940fd5fed65b95e3dee
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-28 04:56:09 +0300

    28.03.2023

--- src/day27_Constructor/C02_Constructor.java
+++ src/day27_Constructor/C02_Constructor.java
@@ -1,4 +1,17 @@
 package day27_Constructor;
 
 public class C02_Constructor {
+    public static void main(String[] args) {
+        C01         obj1        =   new         C01();
+        // Class adi -- objenin adi -- keyborad -- constructor
+
+        /*
+        Java da bir obje olusturabilmek icin mutlaka constructor kullanmalisiniz.
+         */
+
+
+        System.out.println(obj1.sayi);
+        obj1.deneme(); // C01'den deneme methodu calisir.
+
+    }
 }
