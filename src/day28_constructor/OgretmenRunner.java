001ce796c7925c9c19a4d456d817e44ade0446b7
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-29 04:06:10 +0300

    29.03.2023

--- src/day28_constructor/OgretmenRunner.java
+++ src/day28_constructor/OgretmenRunner.java
@@ -9,5 +9,9 @@ public class OgretmenRunner {
 
         Ogretmen ogretmen2=new Ogretmen("Emre","Akdoğan","1/1/2001");
         System.out.println("Ogretmen 2"+ ogretmen2);
+
+        Ogretmen ogretmen3=new Ogretmen("Cavidan","Kabinkara","1/1/1997");
+        System.out.println("Ogretmen 3"+ ogretmen3);
+
     }
 }
