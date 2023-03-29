8b9008a611949cbcce6cc64421851b603663545d
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-30 00:39:19 +0300

    30.03.2023

--- src/day30_staticBlock/C03_PassByValue.java
+++ src/day30_staticBlock/C03_PassByValue.java
@@ -7,13 +7,14 @@ public class C03_PassByValue {
          */
 
         double satisFiyatı=100;
+        double İndirimOranı=25;
 
-        double indirimliFiyat=indirimliFiyatHesapla(satisFiyatı);
+        double indirimliFiyat=indirimliFiyatHesapla(satisFiyatı,İndirimOranı);
         System.out.println("indirimli Fiyati : "+indirimliFiyat);
     }
 
-    public static double indirimliFiyatHesapla(double satisFiyatı) {
-        double indirimliFiyat=satisFiyatı*0.75;
+    public static double indirimliFiyatHesapla(double satisFiyatı,double İndirimOranı) {
+        double indirimliFiyat=satisFiyatı*(1-İndirimOranı/100);
 
         return indirimliFiyat;
     }
