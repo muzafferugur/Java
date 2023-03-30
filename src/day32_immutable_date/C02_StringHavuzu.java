0f205407334be3f9ebda644055673e858f3734d2
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-31 00:08:38 +0300

    31.03.2023

--- src/day32_immutable_date/C02_StringHavuzu.java
+++ src/day32_immutable_date/C02_StringHavuzu.java
@@ -17,7 +17,34 @@ public class C02_StringHavuzu {
         System.out.println(str2.equals(str4));//true
         System.out.println(str2==str4);//false
 
+        /*
+        1- Yeni bir String olustururken esitligin saginda new keyword'u olursa
+            java direk yeni bir obje ve referans olusturur.
+        2- Eger esitligin sagina bir method calisiyor veya + islemi yapiliyorsa
+           String immutable oldugundan degisikligi kaydetmek uzere hemen bir kopya
+           String ve referans olusturur, sonra degeri hesaplayip bu yeni kopya
+           icine koyar
+         */
 
+        String str5="Ali Can";
+        String str6=str1;
+
+        System.out.println(str1.equals(str5));//true
+        System.out.println(str5==str1);//true
+
+        System.out.println(str1.equals(str6));//true
+        System.out.println(str1==str6);//true
+        System.out.println(str5.equals(str6)); // true
+        System.out.println(str5 == str6); // true
+
+
+        /*
+        1- Eger yeni string objesi olusturulurken new kelimesi kullanilmaz veya
+           esitligin saginda bir (+) islemi olmazsa Java bakar
+        1- Eger daha once olusturulan String objelerden (String Havuzu)
+           bire bir ayni String varsa o objeyi ve referansini kullanir.
+           Birebir aynisi yoksa yeni bir obje olusturur.
+         */
 
     }
 }
---
+++ src/day32_immutable_date/C03_localdate.java
@@ -0,0 +1,8 @@
+package day32_immutable_date;
+
+public class C03_localdate {
+
+    public static void main(String[] args) {
+
+    }
+}
