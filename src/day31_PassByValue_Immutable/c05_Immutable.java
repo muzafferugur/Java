57b1676e745f3f0fef4d534645c338cab68dd984
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-30 05:08:09 +0300

    30.03.2023

---
+++ src/day31_PassByValue_Immutable/c05_Immutable.java
@@ -0,0 +1,19 @@
+package day31_PassByValue_Immutable;
+
+public class c05_Immutable {
+    public static void main(String[] args) {
+        String isim = "Mine";
+
+        isim = isim.toUpperCase();
+        System.out.println(isim); // MINE
+
+        isim = "Mevlüt";
+        System.out.println(isim); // Mevlüt
+
+        for (int i = 0; i < 100; i++) {
+            isim+=".";
+
+        }
+        System.out.println(isim);
+    }
+}
