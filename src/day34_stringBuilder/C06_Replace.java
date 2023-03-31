e2530f790278d4d663267abe1707cf927f794a3c
MuzafferUgur <av.muzafferugur@gmail.com>
2023-03-31 23:55:31 +0300

    31.03.2023

--- src/day34_stringBuilder/C06_Replace.java
+++ src/day34_stringBuilder/C06_Replace.java
@@ -4,5 +4,20 @@ public class C06_Replace {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Calm down");
 
+        sb.replace(5,10,"Up");
+
+        System.out.println(sb);//Calm Up
+
+        sb.replace(4,5,"---");
+        System.out.println(sb);//Calm---Up
+
+        sb.replace(4,7," ");
+        System.out.println("sb = " + sb); // sb = Calm Up
+
+        sb.replace(5,7,"Down");
+        System.out.println("sb = " + sb); // sb = Calm Down
+
+
+
     }
 }
