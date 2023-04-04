6145fc7f78ef833981a95c3cf2cb84e52cd997cd
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-04 03:58:16 +0300

    04.04.2023

--- src/day41_Exception/C01_IOExceptions.java
+++ src/day41_Exception/C01_IOExceptions.java
@@ -1,7 +1,44 @@
 package day41_Exception;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class C01_IOExceptions {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        /*
+        Java'da bilgisayarimizdaki bir dosyaya erismek istiyorsak
+        FileInputStream Class'indan yardim aliriz.
+
+        Ayni sekilde Java'dan bilgisayarimizdaki bir dosyaya ekleme veya uptade yapmak
+        istersek FileOutputStream Class'indan yardim aliriz.
+         */
+
+        FileInputStream fis=new FileInputStream("src/day41_Exception/Test.txt");
+
+        /*
+        --> Goruldugu gibi compile time'da altini kirmizi cizen her durum
+            Compile Time Error degildir.
+
+        -->Java'da bazi exception'lar da Compile Time Exception'dir
+
+        --> Ozellikle dosya okuma ve yazma ile ilgili exception'lar
+            Compile Time Exception'dir.
+
+        --> Compile Time Exception olusutugunda java cozum icin 2 ihtimal onerir.
+            1- try - catch ile cevrelemek
+            2- method signature'ina throws keyword ile beklenen exception turunu yazmak
+
+        --> Throws exception, sadece oalyin farkinda oldugumuzun deklarasyonudur
+            exception'in handle edilmesinde hicbir rolu yoktur
+
+        --> yani _try - catch_ ile kontrol altina aldigimiz(handle) exception'lar da kod calismaya
+            devam ediyordu.
+
+        --> Ancak throws Exception yazdigimiza Java bir exception ile karşilasirsa hicbir
+            sey yapmamisiz gibi hata mesaji yayinlayip calismayi durdurur.
+         */
+
 
     }
 }
---
+++ src/day41_Exception/C02_IcIceTryCatch.java
@@ -0,0 +1,39 @@
+package day41_Exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C02_IcIceTryCatch {
+    public static void main(String[] args) {
+        int k;
+
+        try {
+            FileInputStream fis=new FileInputStream("src/day41_Exception/Test.txt");
+
+       while ((k=fis.read()) !=-1){
+           System.out.print((char) k);
+       }
+
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya bulunamadı ");
+
+            /*
+            IOException-->parent
+            FileNotFoundException-->child
+            birden fazla bir birine bağlı exception beklediğimizde try-catch yonteminde
+            önce child exception sonra parent exception yazılır
+            önce parent yazılırsa en kapsayıcı olduğundan child a gerek kalmaz ve
+            child ı sonraya yazmak istersek hatalı olur
+
+            birbirinden bağımsız exceptionlar beklediğimizde istediğimiz sırayla yazabiliriz
+
+             */
+
+        } catch (IOException e) {
+            System.out.println("dosyadan bilgiler okunamadı");
+        }
+    }
+}
