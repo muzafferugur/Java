cd4b35ad5ae76c1708407a97e3fd8801aa2b41d8
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-05 03:57:13 +0300

    05.04.2023

--- src/day44_AbstractClass_Interfaces/I02_Interfaces.java
+++ src/day44_AbstractClass_Interfaces/I02_Interfaces.java
@@ -6,12 +6,12 @@ public interface I02_Interfaces {
     /*
     Interface bir class degildir.
     Abstract Class'lar java'da abstraction (Soyutlaştırma / Kural koyma)
-    isleviini yapiyordu.
+    islevini yapiyordu.
 
-    Ancak abstract bir class'da absttract olmayan method'lar da olabilir. bu da full
-    absttraction yapmaya izin vermez.
+    Ancak abstract bir class'da abstract olmayan method'lar da olabilir. bu da full
+    abstraction yapmaya izin vermez.
 
-    Eger javai2da icinde bir concrete method olmasin dedigimiz bir class olusturmak
+    Eger java'da icinde bir concrete method olmasin dedigimiz bir class olusturmak
     istiyorsaniz bunu class degiil Interface yapmalisiniz.
 
         1- INTERFACE'LERDE CONCRETE METHOD OLMAZ.
@@ -20,11 +20,14 @@ public interface I02_Interfaces {
             List<String> liste = new List<String>(); --> calismaz
             Bunun yerine constructor'i list'in child'i olan ArrayList'den seceriz.
             List<String> liste = new ArrayList<String>(); --> calisir.
-        4- Class'larda bir child birden fazla parent edilemez.
+        4- Class'larda bir child birden fazla parent edinemez.
             ancak Interface'lerde concrete method olmadigindan biz her methodu child
             class'da override etmek zorundayiz
             Override ederken kimin soyledigini override ettigimizin onemi yok.
         5- Interface'lar neyin yapilmasini gerektigini soyler ama nasil
             yapilacagina karismaz.
+
+            interface bir çeşit "to do list"tir.neyin yapılması gerektiğini söyler.ne ypılması gerektiği
+            değil ne yapılması gerektiği.
      */
 }
