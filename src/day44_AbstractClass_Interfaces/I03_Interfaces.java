af7b518a18481e75f85a1f3774ac0e428a9c12c3
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-05 04:06:06 +0300

    05.04.2023

--- src/day44_AbstractClass_Interfaces/I03_Interfaces.java
+++ src/day44_AbstractClass_Interfaces/I03_Interfaces.java
@@ -14,12 +14,14 @@ public interface I03_Interfaces {
     /*
     Interface icerisindeki her method public abstract ozelliklerine sahiptir.
     abstract bir method'un body'si olmasi mumkun degildir.
+
     Java sonradan devoloper'larin istegi uzerine kısmi bir uptade yapmistir.
     Bir ınterface'e sonradan bir abstract method eklerseniz o ınterface'i daha once
     implement etmis tum class'lara gidip hepsinde yeni eklenen method'u override
     etmeniz gerekir.
+
     Bu da buyuk ve eskiden gelen projeler icin zor bir islemdir.
-    Bunun icin Java8'den itibaren ınterface'lere sonradan body'si olan method
+    Bunun icin Java'den itibaren ınterface'lere sonradan body'si olan method
     eklenmesine izin vermistir.
      */
 
