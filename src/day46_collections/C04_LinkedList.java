423ae530ddd404fc185b40b88df0e27f4f7dc741
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-07 01:17:53 +0300

    07.04.2023

---
+++ src/day46_collections/C04_LinkedList.java
@@ -0,0 +1,21 @@
+package day46_collections;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class C04_LinkedList {
+    public static void main(String[] args) {
+
+
+        LinkedList<String> ll1 = new LinkedList();
+
+        List<String> ll2 = new LinkedList();
+
+        Queue<String> ll3 = new LinkedList();
+
+        Deque<String> ll4 = new LinkedList();
+
+    }
+}
