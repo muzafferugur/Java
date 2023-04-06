a112ac8612a6e9042c36fa2ce21e60853bf59469
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-06 13:26:57 +0300

    06.04.2023

---
+++ src/day46_collections/C01_ListIteratör.java
@@ -0,0 +1,46 @@
+package day46_collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class C01_ListIteratör {
+    public static void main(String[] args) {
+        /*
+        Soru 3) Bir listedeki istenen sayi araliginda olmayan elementleri silen
+        bir program yaziniz …
+        (2. liste olusturmadan, gecerli liste uzerinde islem yapiniz)
+        Orn : [2,13,56,23,45,14,40] istenen aralik 20 ile 40 arasi (sinirlar dahil)
+        output: [23,40]
+         */
+        List<Integer> liste=new ArrayList<>();
+        liste.add(2);
+        liste.add(13);
+        liste.add(56);
+        liste.add(23);
+        liste.add(45);
+        liste.add(14);
+        liste.add(40);
+
+        System.out.println("ilk liste : "+liste);
+
+        ListIterator list1= liste.listIterator();
+       int basSinir=20;
+       int sonSinir=40;
+
+
+
+
+        while(list1.hasNext()){
+            Integer temp=(Integer)list1.next();
+
+            if (temp<basSinir || temp>sonSinir){
+                list1.remove();
+            }
+
+        }
+        System.out.println("liste son hali:"+liste);
+    }
+
+}
---
+++ src/day46_collections/C03_ListIteratörHocaÇözmedi.java
@@ -0,0 +1,50 @@
+package day46_collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class C03_ListIteratörHocaÇözmedi {
+    public static void main(String[] args) {
+        /*
+       (literatör ile index kullanımına örnek) bir listdeki ilk n elemanı iteratör kullanarak 5 artırın
+        örn:
+        list:[2,13,56,23,45,14,40]
+        artırılması istenen eleman sayısı 3
+        output:[7,18,61,23,45,14,40]
+         */
+        List<Integer> liste = new ArrayList<>();
+        liste.add(2);
+        liste.add(13);
+        liste.add(56);
+        liste.add(23);
+        liste.add(45);
+        liste.add(14);
+        liste.add(40);
+
+        System.out.println("liste ilk hali : "+liste);//[2, 13, 56, 23, 45, 14, 40]
+        Integer value;
+        int n = 3;
+        ListIterator list1= liste.listIterator();
+        while (n > 0 && list1.hasNext()) {
+            value =(Integer) list1.next();
+            list1.remove();
+            list1.add(value + 5);
+            n--;
+        }
+
+        System.out.println("liste ilk 3 elamanına +5 hali : "+liste);//[7, 18, 61, 23, 45, 14, 40]
+    }
+
+    /*
+    öncelikle List sınıfından bir liste oluşturuluyor ve elemanlar ekleniyor. Daha sonra n değişkeni ile
+    kaç elemanın artırılacağı belirtiliyor ve ListIterator sınıfından bir (LİST1)iteratör oluşturuluyor.
+
+    Daha sonra, while döngüsü ile listenin ilk n elemanı üzerinde geziniliyor ve her eleman 5 artırılıyor.
+    İteratör üzerindeki elemanlar silinip, yerlerine artırılmış elemanlar ekleniyor.
+
+    Son olarak, listenin tüm elemanları ekrana yazdırılıyor.
+    Bu şekilde, listenin ilk n elemanı 5 artırılmış olarak elde edilmiş oluyor.
+     */
+}
