8b46bc1726ff4a2c8387c4f24bbf636a3f329865
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-07 01:41:19 +0300

    07.04.2023

--- src/day46_collections/C06_LinkedList_DequeDataTuru.java
+++ src/day46_collections/C06_LinkedList_DequeDataTuru.java
@@ -13,8 +13,11 @@ public class C06_LinkedList_DequeDataTuru {
         ll4.add("Tevfik");
         ll4.add("Selim");
 
-        ll4.removeLastOccurrence("Selim");
+        ll4.removeLastOccurrence("Selim");//iki tane selim var-->birden fazla varsa son kullanılanı sil
         System.out.println(ll4); // [Cavidan, Mesut, Selim, Tevfik]
         System.out.println(ll4.pop()); // Cavidan
+
+        //queue de eklenen sona eklenir silinen baştan silinir bunda bastan ve sondan ekleme ve silme methodları var
+        //bastakini getir sondakini getir.
     }
 }
