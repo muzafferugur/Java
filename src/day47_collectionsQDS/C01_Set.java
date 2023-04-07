29c0f7f66c8a0191f60628b39599e254f4beb918
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-07 03:13:12 +0300

    07.04.2023

--- src/day47_collectionsQDS/C01_Set.java
+++ src/day47_collectionsQDS/C01_Set.java
@@ -8,6 +8,7 @@ public class C01_Set {
     public static void main(String[] args) {
         /*
         Set (Küme)-->elemanları uniquedir.Tekrarsız olsun diye setten yaptık soruyu
+        java elemanları unique yapmak için hash algoritması kullanır
          */
 
         /*
@@ -15,13 +16,15 @@ public class C01_Set {
         halini array'e atayan kod yaziniz.
          */
         int arr[] = {4, 5, 3, 6, 8, 5, 1, 9, 0, 4, 2, 5, 7, 9, 1, 2, 5, 7, 6};
+        //var olan array e eleman atanamaz o yuzden sıfırdan array oluşturduk
 
         Set<Integer> tekrarsızSet=new HashSet<>();//hızlı olsun diye hash kullanalım
         //SIRALAMAYA İHTİYACIM OLMADIĞI İÇİN FOREACH KULLANDIM-ARRAYDEN ALIP SET E ATICAM
+        //FORLOOP da sürekli kontrol etme ihtiyacı duyuyoruz(if yapısıyla) o yuzden kullanmadık-
 
         for (int each:arr
              ) {
-            tekrarsızSet.add(each);//her bir elementi alıp sete attım
+            tekrarsızSet.add(each);//her bir elementi alıp sete attım tekrarlardan kurtuldum
         }
 
         System.out.println("set : "+tekrarsızSet);// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
