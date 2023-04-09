0c3e497eae16a3a5957b16c7a2cd835a3e7acf71
MuzafferUgur <av.muzafferugur@gmail.com>
2023-04-10 01:50:33 +0300

    10.04.2023

---
+++ src/day17_overloading_ForLoop/Test01_ForLoop.java
@@ -0,0 +1,17 @@
+package day17_overloading_ForLoop;
+
+public class Test01_ForLoop {
+    public static void main(String[] args) {
+        //ekrana 10 kez "Java guzeldir" yazdirin
+
+
+        for (int i =0 ; i<10; i++) {
+            System.out.println("Java guzeldir");
+        }
+
+
+
+
+    }
+
+}
